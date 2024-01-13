6c27231a7bd87c633bb320e59313cdd72173ed69
Beyrem Oueslati <beyrem.weslati@gmail.com>
2024-01-13 20:16:33 +0100

    Updating Script to work with A tags with the dynamic pages and updating the photos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,47 @@
 
+function loadPage(targetPage) {
+    console.log(targetPage);
+    fetch(targetPage)
+        .then(response => response.text())
+        .then(html => {
+            // Replace the content of the main section with the loaded content
+            document.querySelector('.page-main').innerHTML = html;
+            runMainScript();
+            document.querySelector('.page-main').addEventListener('click', function(event) {
+                const target = event.target.closest('a');
+                // Check if the clicked element is an A tag with a specific class or other criteria
+                if (target.id === 'GameProfile') {
+                    event.preventDefault(); // Prevent the default behavior of the link
+                    const targetPage = target.getAttribute('href');
+                    loadPage(targetPage); // Load the new page
+                }
+            });
+        })
+        .catch(error => console.error('Error loading page:', error));
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    function loadPage(targetPage) {
-        const previousActiveLink = document.querySelector('.uk-nav .uk-active');
+function ActivateClass(targetPage){
+    const previousActiveLink = document.querySelector('.uk-nav .uk-active');
         if(previousActiveLink){
             previousActiveLink.classList.remove("uk-active");
+        }   
+        const NextActiveLink = document.querySelector(`a[href="${targetPage}"]`);
+        if(NextActiveLink){
+            NextActiveLink.parentNode.classList.add("uk-active");
         }
-        fetch(targetPage)
-            .then(response => response.text())
-            .then(html => {
-                // Replace the content of the main section with the loaded content
-                document.querySelector('.page-main').innerHTML = html;
-                const NextActiveLink = document.querySelector(`a[href="${targetPage}"]`);
-                if(NextActiveLink){
-                    NextActiveLink.parentNode.classList.add("uk-active");
-                }
-                runMainScript();
-            })
-            .catch(error => console.error('Error loading page:', error));
-    }
+}
 
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.uk-nav').addEventListener('click', function (event) {
         event.preventDefault();
         const link = event.target.closest('a');
         if(link){
             const targetPage = link.getAttribute('href');
             loadPage(targetPage);
-        }
+            ActivateClass(targetPage);
+        }  
     });
 
     loadPage("03_home.html");
+    ActivateClass("03_home.html");
 });
