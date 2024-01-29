bc195299eaf76b2ce3230617e69ca179e6905b78
Beyrem Oueslati <beyrem.weslati@gmail.com>
2024-01-29 23:03:25 +0100

    renamed functions and variable, refactoring larger functions into smaller ones for clarity

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,7 +25,8 @@ async function fetchLocalGameData(gameId) {
     }
 }
 
-async function updatePage(gameData,imgData) {
+// Store Page content
+async function updateContentWithGameData(gameData,imgData) {
     document.querySelector('h3#gameTitle').textContent = gameData.name;
     document.querySelector('.game-profile-card__intro span').innerHTML = gameData.description;
     document.querySelector('.game-profile-card__list li:nth-child(2) div:nth-child(2)').textContent = gameData.released;
@@ -43,17 +44,13 @@ async function updatePage(gameData,imgData) {
     SmallgalleryContainer.querySelector('.swiper-slide:nth-child(1) img').src = gameData.background_image;
 
     const imageUrls = imgData.results.map(result => result.image);
-    
-    
 
     if (imageUrls && imageUrls.length >= 4) {
         for(let i=0;i<=3;i++){
             BigGalleryContainer.querySelector(`.swiper-slide:nth-child(${i+2}) img`).src = imageUrls[i];
             SmallgalleryContainer.querySelector(`.swiper-slide:nth-child(${i+2}) img`).src = imageUrls[i];
-        }
-        
+        }   
     }
-        
     const localGame = await fetchLocalGameData(gameData.id); 
     if (localGame) {
         document.querySelector('.game-profile-price__value').textContent = `${localGame.price} TND`;
@@ -62,7 +59,7 @@ async function updatePage(gameData,imgData) {
 }
 
 const marketCache = {};
-
+// Fetching game data of the store page using RawgApi
 async function fetchGameDetails(gameId) {
     if(marketCache[gameId]){
         return marketCache[gameId];
@@ -83,34 +80,44 @@ async function fetchGameDetails(gameId) {
         }
     }
 }
+
+// Fetching Price of game currrency of market page using the local data
 async function fetchMarketPrices(gameId) {
     const localMarket = 'assets/DB/localMarketData.json';
     try {
         const response = await fetch(localMarket);
         const data = await response.json();
-        const gameData = data.find(game => game.gameTitle === gameId);
-        if(gameData){
-            document.querySelector('.game-card__media2 img').src = gameData.ImgSrc;
-            document.querySelector('.game-profile-card__intro span').innerHTML = gameData.description;
-            document.querySelector('.game-profile-card__list li:nth-child(1) div:nth-child(2)').textContent = gameData.releaseDate;
-            document.querySelector('.game-profile-card__list li:nth-child(2) div:nth-child(2)').textContent = gameData.developer;
-            const optionsContainer = document.querySelector('.toggle');
-            optionsContainer.innerHTML = `
-                ${gameData.options.map((option, i) => `
-                    <input id="input_${i}" type="radio" name="quantity" value="${option.price}" ${option.selected ? 'checked' : ''}>
-                    <label for="input_${i}" class="radioLabel">${option.quantity} ${gameData.currency} </label><br>`).join('')}
-                `;
-            var quantityRadios = document.querySelectorAll('input[name="quantity"]');
-            quantityRadios.forEach(function (radio) {
-                radio.addEventListener('change', function () {
-                    document.querySelector(".game-profile-price__value").innerHTML = `${this.value} TND`
-                });
-            });
+        const currencyData = data.find(game => game.gameTitle === gameId);
+        if(currencyData){
+            return currencyData;
         }
     } catch (error) {
         console.error(error);
+        return null;
     }
 }
+
+// updating currency values  
+function updateContentWithCurrencyData(currencyData){
+    document.querySelector('.game-card__media2 img').src = currencyData.ImgSrc;
+    document.querySelector('.game-profile-card__intro span').innerHTML = currencyData.description;
+    document.querySelector('.game-profile-card__list li:nth-child(1) div:nth-child(2)').textContent = currencyData.releaseDate;
+    document.querySelector('.game-profile-card__list li:nth-child(2) div:nth-child(2)').textContent = currencyData.developer;
+    const optionsContainer = document.querySelector('.toggle');
+    optionsContainer.innerHTML = `
+        ${currencyData.options.map((option, i) => `
+            <input id="input_${i}" type="radio" name="quantity" value="${option.price}" ${option.selected ? 'checked' : ''}>
+            <label for="input_${i}" class="radioLabel">${option.quantity} ${currencyData.currency} </label><br>`).join('')}
+        `;
+    var quantityRadios = document.querySelectorAll('input[name="quantity"]');
+    quantityRadios.forEach(function (radio) {
+        radio.addEventListener('change', function () {
+            document.querySelector(".game-profile-price__value").innerHTML = `${this.value} TND`
+        });
+    });
+}
+
+// Getting data of every game from rawg api and filling the page 
 function fillGameCards(containerClass,subContainerClass){
     const gameCards = document.querySelectorAll(`.${containerClass} .${subContainerClass}`);
     gameCards.forEach(async (card) => {
@@ -118,86 +125,118 @@ function fillGameCards(containerClass,subContainerClass){
         const {gameData,imgData} = await fetchGameDetails(gameId);
 
         if(gameData){
-            card.querySelector(".game-card__media img").src = gameData.background_image;
-            card.querySelector(".game-card__info .game-card__title").textContent = gameData.name;
-            card.querySelector(".game-card__info .game-card__rating-and-price .game-card__rating span").textContent = gameData.rating;
-            card.querySelector(".game-card__info .game-card__genre span:nth-child(1)").textContent = gameData.genres[0].name;
-            if(gameData.tags[0]){
-                card.querySelector(".game-card__info .game-card__genre span:nth-child(2)").textContent = "/";
-                card.querySelector(".game-card__info .game-card__genre span:nth-child(3)").textContent = gameData.tags[0].name;
-            }
-            
-            const localGame = await fetchLocalGameData(gameData.id);
-            card.querySelector(".game-card__info .game-card__rating-and-price .game-card__price span").textContent = `${localGame.price} TND`;
+            renderGameCard(card, gameData);
         }
     })
 }
+
+// Render game cards with their content 
+async function renderGameCard(card , gameData){
+    
+    card.querySelector(".game-card__media img").src = gameData.background_image;
+    card.querySelector(".game-card__info .game-card__title").textContent = gameData.name;
+    card.querySelector(".game-card__info .game-card__rating-and-price .game-card__rating span").textContent = gameData.rating;
+    card.querySelector(".game-card__info .game-card__genre span:nth-child(1)").textContent = gameData.genres[0].name;
+    if(gameData.tags[0]){
+        card.querySelector(".game-card__info .game-card__genre span:nth-child(2)").textContent = "/";
+        card.querySelector(".game-card__info .game-card__genre span:nth-child(3)").textContent = gameData.tags[0].name;
+    }
+    
+    const localGame = await fetchLocalGameData(gameData.id);
+    card.querySelector(".game-card__info .game-card__rating-and-price .game-card__price span").textContent = `${localGame.price} TND`;
+} 
+
 
 const pageCache = {};
 
 async function loadPage(targetPage, previousPage) {
     try {
-        let html;
-        if (pageCache[targetPage]) {
-            html = pageCache[targetPage];
-        } else {
-            const response = await fetch(targetPage);
-            html = await response.text();
-            pageCache[targetPage] = html;
-        }
+        const html = await getPageHTML(targetPage);
 
-        document.querySelector('.page-main').innerHTML = html;
-        const backbutton = document.querySelector(".back");
-        if (backbutton) {
-            backbutton.href = previousPage;
-        }
+        renderHTML(html);
+        setBackButton(previousPage);
         runMainScript();
 
         const currentPage = targetPage;
-        if(currentPage == "Home.html"){
-            fillGameCards("js-popular","swiper-slide");
+        if (currentPage === "Home.html") {
+            fillGameCards("js-popular", "swiper-slide");
         }
-        if(currentPage == "Store.html"){
-            fillGameCards("CardsContainer","Wrapper");
-        }
-        const pageMain = document.querySelector('.page-main');
-        if (pageMain._clickHandler) {
-            pageMain.removeEventListener('click', pageMain._clickHandler);
+        if (currentPage === "Store.html") {
+            fillGameCards("CardsContainer", "Wrapper");
         }
 
-        pageMain._clickHandler = async function (event) {
-            event.stopPropagation();
-            const target = event.target.closest('a');
-
-            if (target != null && target.id === 'GameProfile') {
-                event.preventDefault();
-                const targetPage = target.getAttribute('href');
-
-                await loadPage(targetPage, currentPage);
-
-                if (target.getAttribute("data-game-id") != null) {
-                    const gameId = target.getAttribute("data-game-id");
-                    const {gameData, imgData} = await fetchGameDetails(gameId);
-                    updatePage(gameData, imgData);
-                }
-            }
-            if (target != null && target.id === 'GameMarket') {
-                event.preventDefault();
-                const targetPage = target.getAttribute('href');
-
-                await loadPage(targetPage, currentPage);
-
-                if (target.getAttribute("data-game-id") != null) {
-                    const gameId = target.getAttribute("data-game-id");
-                    fetchMarketPrices(gameId);
-                }
-            }
-        };
-        pageMain.addEventListener("click", pageMain._clickHandler);
+        setupClickHandler(currentPage);
     } catch (error) {
         console.error('Error loading page:', error);
     }
 }
+
+async function getPageHTML(targetPage) {
+    if (pageCache[targetPage]) {
+        return pageCache[targetPage];
+    } else {
+        const response = await fetch(targetPage);
+        const html = await response.text();
+        pageCache[targetPage] = html;
+        return html;
+    }
+}
+
+function renderHTML(html) {
+    document.querySelector('.page-main').innerHTML = html;
+}
+
+function setBackButton(previousPage) {
+    const backbutton = document.querySelector(".back");
+    if (backbutton) {
+        backbutton.href = previousPage;
+    }
+}
+
+function setupClickHandler(currentPage) {
+    const pageMain = document.querySelector('.page-main');
+    if (pageMain._clickHandler) {
+        pageMain.removeEventListener('click', pageMain._clickHandler);
+    }
+
+    pageMain._clickHandler = async function (event) {
+        event.stopPropagation();
+        const target = event.target.closest('a');
+
+        if (target != null && target.id === 'GameProfile') {
+            event.preventDefault();
+            handleGameProfileClick(target, currentPage);
+        }
+        if (target != null && target.id === 'GameMarket') {
+            event.preventDefault();
+            handleGameMarketClick(target, currentPage);
+        }
+    };
+    pageMain.addEventListener("click", pageMain._clickHandler);
+}
+
+async function handleGameProfileClick(target, currentPage) {
+    const targetPage = target.getAttribute('href');
+    await loadPage(targetPage, currentPage);
+
+    if (target.getAttribute("data-game-id") != null) {
+        const gameId = target.getAttribute("data-game-id");
+        const {gameData, imgData} = await fetchGameDetails(gameId);
+        updateContentWithGameData(gameData, imgData);
+    }
+}
+
+async function handleGameMarketClick(target, currentPage) {
+    const targetPage = target.getAttribute('href');
+    await loadPage(targetPage, currentPage);
+
+    if (target.getAttribute("data-game-id") != null) {
+        const gameId = target.getAttribute("data-game-id");
+        const currencyData = await fetchMarketPrices(gameId);
+        updateContentWithCurrencyData(currencyData);
+    }
+}
+
 
 function ActivateClass(targetPage) {
     const previousActiveLink = document.querySelector('.uk-nav .uk-active');
